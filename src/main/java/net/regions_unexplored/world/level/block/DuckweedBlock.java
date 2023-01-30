46248416c4e785846dd9e85acbc86f064e7066fc
Eric Adams <ericadams8@yahoo.ca>
2023-01-30 05:36:23 -0500

    Fixed Duckweed planting on everything

--- src/main/java/net/regions_unexplored/world/level/block/DuckweedBlock.java
+++ src/main/java/net/regions_unexplored/world/level/block/DuckweedBlock.java
@@ -1,13 +1,17 @@
 package net.regions_unexplored.world.level.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.PushReaction;
 
@@ -37,6 +41,20 @@ public class DuckweedBlock extends WaterlilyBlock {
     @Override
     public boolean canBeReplaced(BlockState state, BlockPlaceContext context) {
         return context.getItemInHand().getItem() != this.asItem();
+    }
+
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        BlockPos blockpos = pos.below();
+
+        return this.mayPlaceOn(level.getBlockState(blockpos), level, blockpos);
+    }
+
+    @Override
+    protected boolean mayPlaceOn(BlockState state, BlockGetter getter, BlockPos pos) {
+        FluidState fluidState = getter.getFluidState(pos);
+        FluidState fluidState1 = getter.getFluidState(pos.above());
+        return (fluidState.getType() == Fluids.WATER || state.getMaterial() == Material.ICE) && fluidState1.getType() == Fluids.EMPTY;
     }
 
     @Override
